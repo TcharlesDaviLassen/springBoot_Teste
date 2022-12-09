daa5712f02b1401086323531ce0d424ae5e25e30
TcharlesDaviLassen <tcharlesdavilassen@gmail.com>
2022-12-09 08:46:47 -0300

    controller

--- src/main/java/com/example/testeSpring/controller/HomeController.java
+++ src/main/java/com/example/testeSpring/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // Associada com classes que possuem métodos que processam requests numa aplicação web.
 @RequestMapping("/") // @RequestMapping permite definir uma rota. Caso não seja informado o método HTTP da rota, ela será definida para todos os métodos.
 public class HomeController {
 
